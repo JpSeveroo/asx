a67f9e82ddd53d8a38e35a88fdcbe9c718652c44
João Pedro Severo <142758081+JpSeveroo@users.noreply.github.com>
2026-08-03 19:51:43 -0300

    Corrigindo sessao

--- src/main/java/com/group/asx/domain/Sessao.java
+++ src/main/java/com/group/asx/domain/Sessao.java
@@ -1,11 +1,18 @@
 package com.group.asx.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
 @Table(name = "sessao")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Sessao {
 
     @Id
@@ -25,43 +32,14 @@ public class Sessao {
     @Column(nullable = false)
     private boolean revogado;
 
-    public UUID getId() {
-        return id;
+    @Column(nullable = false, updatable = false)
+    private Instant dataCriacao;
+
+    public boolean estaExpirada() {
+        return Instant.now().isAfter(dataExpiracao);
     }
 
-    public Investidor getInvestidor() {
-        return investidor;
-    }
-
-    public void setInvestidor(Investidor investidor) {
-        this.investidor = investidor;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Instant getDataExpiracao() {
-        return dataExpiracao;
-    }
-
-    public void setDataExpiracao(Instant dataExpiracao) {
-        this.dataExpiracao = dataExpiracao;
-    }
-
-    public boolean isRevogado() {
-        return revogado;
-    }
-
-    public void setRevogado(boolean revogado) {
-        this.revogado = revogado;
+    public boolean estaValida() {
+        return !revogado && !estaExpirada();
     }
 }
