5b594f690628c7d03af7a001806ada5c276b7834
Gustavo0606 <gustavovilelas22@gmail.com>
2026-08-05 10:23:23 -0300

    feat: Cria a validação de cadastro e mensagens de resposa em caso de um envio errado do usuário

--- src/main/java/com/group/asx/dto/CadastroRequestDTO.java
+++ src/main/java/com/group/asx/dto/CadastroRequestDTO.java
@@ -2,14 +2,30 @@ package com.group.asx.dto;
 
 
 import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record CadastroRequestDTO(
-        @NotBlank String nomeInvestidor,
-        @NotBlank String email,
-        @NotBlank String senha,
-        @NotBlank String confirmacaoSenha,
-        @AssertTrue boolean confirmouMaioridade,
-        @AssertTrue boolean aceitouTermos
+        @NotBlank (message = "O nome do investidor é obrigatório")
+        String nomeInvestidor,
+
+        @Email (message = "Formato de e-mail inválido")
+        @NotBlank (message = "O e-mail é obrigatório")
+        String email,
+
+        @NotBlank (message = "A senha é obrigatória")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+                message = "A senha deve conter ao menos uma letra maiúscula, uma minúscula e um número")
+        String senha,
+
+        @NotBlank (message = "A confirmação de senha é obrigatória")
+        String confirmacaoSenha,
+
+        @AssertTrue (message = "Você deve confirmar que é maior de idade")
+        boolean confirmouMaioridade,
+
+        @AssertTrue (message = "Você deve aceitar os termos de uso")
+        boolean aceitouTermos
                                     ) {
 }
